902a3655e374e56ff9c4e26bdaa0833f9822cebf
OlehPotap <potap.oleh.v@gmail.com>
2022-01-27 11:09:00 +0200

    hw11almostDone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 'use strict';
-function ShowStuff(name) {
-  console.log(name);
+import Handlebars from 'handlebars';
+import { FetchImages } from './js/fetchImages.js';
+import imageBoxTamplate from './handlebars/image-tamplate';
+import Notiflix from 'notiflix';
+import { noConflict } from 'handlebars/runtime';
+
+const ImageSerchFormEl = document.querySelector('.search-form');
+const ImageSearchInputEl = document.querySelector('input[name="searchQuery"]');
+const ImageGalleryEl = document.querySelector('.gallery');
+
+ImageSerchFormEl.addEventListener('submit', SubmitRequest);
+
+function SubmitRequest(event) {
+  event.preventDefault();
+  ImageGalleryEl.innerHTML = '';
+
+  FetchImages(ImageSearchInputEl.value)
+    .then(({ hits } = {}) => {
+      if (hits.length == 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+        return;
+      } else if (ImageSearchInputEl.value === '') {
+        Notiflix.Notify.info('Please type something');
+        return;
+      }
+      hits.map(el => {
+        ImageGalleryEl.insertAdjacentHTML('beforeend', imageBoxTamplate(el));
+      });
+    })
+    .catch(err => {
+      console.log('ты напартачил');
+    });
 }
-ShowStuff('здарова');
---
+++ src/js/fetchImages.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const MY_API_KEY = '25419147-5e3b8aa1374258c5e90c55338';
+const OPTIONS = '&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+let pageChanger = 1;
+
+export async function FetchImages(query) {
+  const { data } = await axios.get(
+    `${BASE_URL}?q=${query}${OPTIONS}&page=${pageChanger}&key=${MY_API_KEY}`,
+  );
+  pageChanger += 1;
+  return data;
+}
